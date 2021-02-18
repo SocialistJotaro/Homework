68cd4f438766dc311e37b2defc5b1c25978a76a1
golov <golovenchitc@gmail.com>
2021-02-18 18:05:22 +0300

    Задание 1

--- src/hw4.java
+++ src/hw4.java
@@ -1,3 +1,44 @@
+import java.util.Scanner;
+
 public class hw4 {
-    
+
+    public static void main(String[] args) {
+//        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        numGame();
+
+    }
+
+    static void numGame() {
+        Scanner in = new Scanner(System.in);
+        game:
+        while (true) {
+            System.out.println("Guess the number from 0 to 9.");
+            int true_num = (int) (Math.random() * 10);
+            for (int i = 3; i > 0; i--) {
+                System.out.println(i + " tries remaining.");
+                int user_num = in.nextInt();
+                if (user_num > true_num) {
+                    System.out.println("True number is less.");
+                } else if (user_num < true_num) {
+                    System.out.println("True number is greater.");
+                } else {
+                    System.out.println("You guessed!");
+                    break;
+                }
+            }
+
+            System.out.println("Play again?\n1 - yes / 0 - no");
+            int play = in.nextInt();
+            switch (play) {
+                case 1:
+                    continue;
+                case 0:
+                    break game;
+            }
+
+        }
+    }
 }
+
+
+
